f5194526610db358db578dc42c4440af51b3fc05
David Lozano <david140813@gmail.com>
2020-05-23 16:10:02 -0400

    split two_characters solution in two methods

--- HackerRank/algorithms/strings/two_characters.py
+++ HackerRank/algorithms/strings/two_characters.py
@@ -6,22 +6,26 @@
 
 #!/bin/python3
 
+def pair_substring(s, pair):
+    substring = []
+    for char in s:
+        if char in pair:
+            if not substring or char != substring[-1]:
+                substring.append(char)
+            elif char == substring[-1]:
+                substring = []
+                break
+    return substring
+
+
 def alternate(s):
     unic = sorted(list(set(s)))
     string_length = 0
 
     for index1 in range(len(unic)):
         for index2 in range(index1 + 1, len(unic)):
-            lst = []
-            for char in s:
-                pair = {unic[index1], unic[index2]}
-                char_in_pair = char in pair
-                if char_in_pair:
-                    if not lst or char != lst[-1]:
-                        lst.append(char)
-                    elif char == lst[-1]:
-                        lst = []
-                        break
+            pair = {unic[index1], unic[index2]}
+            lst = pair_substring(s, pair)
             if len(lst) > string_length:
                 string_length = len(lst)
     return string_length
