3667fb5e4ab56704c8b0081dbf4d134666b50d3f
David Lozano <david140813@gmail.com>
2020-06-06 19:58:51 -0400

    update gemstones solution

--- HackerRank/algorithms/strings/gemstones.py
+++ HackerRank/algorithms/strings/gemstones.py
@@ -4,30 +4,8 @@
 # problem url: https://www.hackerrank.com/challenges/gem-stones/problem
 # date: 06/06/2020
 
-def gemstones(rocks):
-    minerals = set()
-    gemsones = 0
-    rock_count = len(rocks)
-    for rock in rocks:
-        minerals.update(set(rock))
-    for mineral in minerals:
-        counter = 0
-        for rock in rocks:
-            if mineral in rock:
-                counter += 1
-                continue
-        if counter ==rock_count:
-            gemsones += 1
-    return gemsones
-
-if __name__ == '__main__':
-
-    n = int(input())
-
-    arr = []
-
-    for _ in range(n):
-        arr_item = input()
-        arr.append(arr_item)
-
-    print(gemstones(arr))
+rock_count = int(input())
+gemstones = set(input())
+for _ in range(rock_count - 1):
+    gemstones = gemstones.intersection(set(input()))
+print(len(gemstones))
