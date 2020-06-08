30b6d068243c0fb51f7469e30f7ef4c9743199cf
David Lozano <david140813@gmail.com>
2020-06-08 00:23:47 -0400

    --amend

--- HackerRank/algorithms/implementation/between_two_sets.py
+++ HackerRank/algorithms/implementation/between_two_sets.py
@@ -6,7 +6,6 @@
 
 def are_factors(iterable, number):
     for factor in iterable:
-        print(f"Checking factors: {number} % {factor} = {number % factor}")
         if number % factor != 0:
             return False
     else:
@@ -14,7 +13,6 @@ def are_factors(iterable, number):
 
 def are_multipes(iterable, number):
     for multiple in iterable:
-        print(f"Checking multiples: {multiple} % {number} = {multiple % number}")
         if multiple % number != 0:
             return False
     else:
@@ -26,10 +24,8 @@ def total_between_two_sets(factors, multiples):
     valid_number = 0
 
     for number in range(max_factor, min_multiple + 1):
-        print(f"MAIN FOR LOOP, TESTING NUMBER: {number}")
         if are_factors(factors, number) and are_multipes(multiples, number):
             valid_number += 1
-        print(f"VALID_NUMBER VALUE: {valid_number}")
     return valid_number
 
 elements = input().split()
